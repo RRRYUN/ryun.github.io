d491824dfb73fc15ae7021a925258503ea3ecbae
Hyeyoon Choi <135585692+RRRYUN@users.noreply.github.com>
2025-05-02 15:18:38 +0900

    Add files via upload

---
+++ portfolio_site/script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mainImage = document.getElementById("main-image");
+  setTimeout(() => {
+    mainImage.classList.add("visible");
+  }, 100);
+});
