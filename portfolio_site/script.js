e7db211a9b0b6670991d8811c15ce2ba70821056
Hyeyoon Choi <135585692+RRRYUN@users.noreply.github.com>
2025-05-02 15:22:44 +0900

    Delete portfolio_site directory

--- portfolio_site/script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const mainImage = document.getElementById("main-image");
-  setTimeout(() => {
-    mainImage.classList.add("visible");
-  }, 100);
-});
